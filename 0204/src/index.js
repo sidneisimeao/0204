d0747fbc7dba19b7cc22ee149d0e7479ad5569fb
Sidnei Simeão <sidneisimmon@gmail.com>
2021-05-13 12:52:11 -0300

    aula 4, crossover rocketseat add editor config

--- 0204/src/index.js
+++ 0204/src/index.js
@@ -1,1 +1,8 @@
-console.log("Somente um teste");
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App() {
+  return React.createContext("a", { href: "https://google.com" }, "Google");
+}
+
+ReactDOM.render("Teste", document.getElementById("root"));
