c824b9d9fabdb2d13834037e20696590cd13f0c2
Sidnei Simeão <sidneisimmon@gmail.com>
2021-05-14 19:00:06 -0300

    0208  - exercicio

--- 0205/src/App.js
+++ 0205/src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Customer from "./Customer";
 
-const App = () => {
-  return <div>Meu App</div>;
+const brenda = {
+  cliente: "Brenda",
+  idade: 26,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 11500" },
+  ],
+  ativa: true,
 };
+
+const sidnei = {
+  cliente: "Sidnei",
+  idade: 34,
+  compras: [
+    { nome: "PC Gamer", preco: "R$ 9000" },
+    { nome: "Monitor", preco: "R$ 300" },
+    { nome: "Caneca", preco: "R$ 15" },
+  ],
+  ativa: false,
+};
+
+const App = () => (
+  <>
+    <Customer {...sidnei} />
+    <hr />
+    <Customer {...brenda} />
+  </>
+);
 
 export default App;
---
+++ 0205/src/Customer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Customer = (props) => {
+  const reducer = (accumulator, { preco }) =>
+    accumulator + Number(preco.replace("R$", ""));
+
+  const total = props.compras.reduce(reducer, 0);
+
+  return (
+    <>
+      <p>Nome: {props.cliente}</p>
+      <p>Idade: {props.idade}</p>
+      <p>
+        Situação:{" "}
+        <span style={{ color: props.ativa ? "green" : "red" }}>
+          {props.ativa ? "Ativa" : "Inativa"}
+        </span>
+      </p>
+      <p>Total gasto: R$: {total}</p>
+      {total > 10000 && <p>Voce gastou muito!</p>}
+    </>
+  );
+};
+
+export default Customer;
